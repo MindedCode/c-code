242fed25c1225b86109a38b22534a320a9a183c2
Adarsh Kumar <99128631+mindedidealcoder@users.noreply.github.com>
2022-04-10 11:46:08 +0530

    Update fib 2.c
    program to calculate the nth number of the fibonacci series

--- recursion/fib 2.c
+++ recursion/fib 2.c
@@ -16,7 +16,14 @@ int fnum(int p)
         return (p);
     else
     return (fnum(p-1)+fnum(p-2));
-}
+    
+            
+            /*p=4; following solve from recursion to answer = fib(4) => 3
+            
+                {(fnum(3)=> 2)                      +         (fnum(2) =>1) ==>2+1=3
+        {fnum(2) => 1   +   fnum(0) => 0 ==>1+1=2 }     {(fnum(1) => 1)   + (fnum(0) => 0) ==> 1+0 = 1}
+    fnum(1) +  fnum(0)   => 1+0 = 1*/
+        }
 
 Output of the program :
 
